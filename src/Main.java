2c454d847afe01a019cb017836758750dfe34f18
Shashika Janith Liyanage <shashika.janith.liyanage@gmail.com>
2023-09-22 08:29:39 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ class Queue{
     }
     private void extendsArray(){
         int[]tempDataArray=new int[dataArray.length+(int)(dataArray.length*loadFactor)];
-        for (int i = 0; i < dataArray.length; i++){
+        for(int i = 0; i < dataArray.length; i++){
             tempDataArray[i]=dataArray[i];
         }
         dataArray=tempDataArray;
@@ -44,29 +44,49 @@ class Queue{
         }
         System.out.println(isEmpty()?"empty]":"\b\b]");
     }
-	/*
-	public String toString(){
 
-	}
-	public int size(){
+    public String toString(){
+        String list="[";
+        for (int i = 0; i < nextIndex; i++)	{
+            list += dataArray[i] + ", ";
+        }
+        return isEmpty() ? "[empty]":list+"\b\b]";
+    }
+    public int size(){
+        return nextIndex;
+    }
+    public int peek(){
+        return isEmpty() ? -1 : dataArray[0];
+    }
+    public int poll() {
+        if(isEmpty()) {
+            return -1;
+        }
+        int firstData = dataArray[0];
+        deQueue();
+        return firstData;
+    }
 
-	}
-	public int peek(){
+    public int[] toArray(){
+        int[] tempDataArray=new int[dataArray.length];
+        for (int i = 0; i < dataArray.length; i++){
+            tempDataArray[i]=dataArray[i];
+        }
+        return tempDataArray;
+    }
+    public int search(int data){
+        for(int i=0; i<nextIndex; i++){
+            if(dataArray[i]==data){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void clear(){
+        dataArray=new int[intSize];
+        nextIndex=0;
+    }
 
-	}
-	public int poll(){
-
-	}
-	public int[] toArray(){
-
-	}
-	public int search(int data){
-
-	}
-	public void clear(){
-
-	}
-	*/
 }
 class Demo{
     public static void main(String args[]){
@@ -77,12 +97,17 @@ class Demo{
         q1.enQueue(40);
         q1.enQueue(50);
         q1.printQueue(); //[10,20,30,40,50]
+        System.out.println();
 
-        q1.deQueue();
+        int data=q1.peek();
+        System.out.println("First element : "+data);
+        System.out.println("After calling peek()....");
+        q1.printQueue(); //[10,20,30,40,50]
+
+        System.out.println();
+        data=q1.poll();
+        System.out.println("First element : "+data);
+        System.out.println("After calling poll()....");
         q1.printQueue(); //[20,30,40,50]
     }
 }
-
-
-
-
